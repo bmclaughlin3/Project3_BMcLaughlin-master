d0cfb455e60ee9a900ad4afeeb4d589c41e4240d
bmclaughlin3 <b4mclaughlin@student.bridgew.edu>
2021-12-08 14:07:05 -0500

    Revision

---
+++ src/ShippingAddress.java
@@ -0,0 +1,31 @@
+
+/**
+ * Class ShippingAddress
+ */
+public class ShippingAddress {
+    private String AddressLine1;
+    private String AddressLine2;
+    private String City;
+    private String State;
+    private String PostalCode;
+    public ShippingAddress (String Line1, String Line2, String city, String State, String postCode) {
+        AddressLine1 = Line1;
+        AddressLine2 = Line2;
+        City = city;
+        this.State = State;
+        PostalCode = postCode;
+    }
+    /**
+     * @return       String
+     */
+    public String toString()
+    {
+        if(AddressLine2.length() > 0)
+            return AddressLine1+'\n'+ AddressLine2+"\n"+City+", "+State+"  "+PostalCode;
+        return AddressLine1+'\n'+City+", "+State+"  "+PostalCode;
+    }
+
+
+
+
+}
